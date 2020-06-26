cb294587c6a5b3a0d84e0dba31be173dbc9f15e7
rushadantia <rushadantia@gmail.com>
2020-06-26 13:36:42 -0400

    button broke

---
+++ Grocery List/View Controllers/HomescreenViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomescreenViewController.swift
+//  Grocery List
+//
+//  Created by Rushad Antia on 6/25/20.
+//  Copyright © 2020 Rushad Antia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomescreenViewController: UIViewController {
+    
+    @IBOutlet weak var storesBtn: UIButton!
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        storesBtn.layer.cornerRadius = 20
+        
+    }
+    
+}
