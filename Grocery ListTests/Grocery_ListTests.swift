e7c0da1bed046d2058cdb966bae6a0843ea8d33a
rushadantia <rushadantia@gmail.com>
2020-07-12 15:28:23 -0400

    added tests but not working

---
+++ Grocery ListTests/Grocery_ListTests.swift
@@ -0,0 +1,24 @@
+//
+//  Grocery_ListTests.swift
+//  Grocery ListTests
+//
+//  Created by Rushad Antia on 7/12/20.
+//  Copyright © 2020 Rushad Antia. All rights reserved.
+//
+
+import XCTest
+
+//@testable import Grocery_List
+
+class Grocery_ListTests: XCTestCase {
+
+    func testHelloWorld(){
+        var hello: String?
+        
+        
+        XCTAssertNil(hello)
+    }
+  
+  
+
+}
