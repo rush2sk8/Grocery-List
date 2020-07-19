94fbe1abe6aaf8067ff5b98099d359abdc89a6de
rushadantia <rushadantia@gmail.com>
2020-07-19 16:23:36 -0400

    actually added a test

--- Grocery ListUITests/Grocery_ListUITests.swift
+++ Grocery ListUITests/Grocery_ListUITests.swift
@@ -9,35 +9,32 @@
 import XCTest
 
 class Grocery_ListUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     
+    override func setUpWithError() throws {
+        continueAfterFailure = false
     }
-
-
-    func testAddStore() throws {
-        // UI tests must launch the application that they test.
+    
+    func testAddStore(){
+        
+        //launch app
         let app = XCUIApplication()
         app.launch()
+        app/*@START_MENU_TOKEN@*/.staticTexts["Stores"]/*[[".buttons[\"Stores\"].staticTexts[\"Stores\"]",".staticTexts[\"Stores\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
-        app.buttons["Stores"].tap()
+        //add store
+        app.navigationBars["Stores"].buttons["Add"].tap()
         
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        //type "wegmans"
+        app.textFields.firstMatch.typeText("Wegmans")
+        XCTAssertTrue((app.textFields.firstMatch.value as! String) == "Wegmans")
+        
+        //add the store
+        app.alerts["Store Name"].scrollViews.otherElements.buttons["Add"].tap()
+        
+        //click on the store
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Wegmans"]/*[[".cells.staticTexts[\"Wegmans\"]",".staticTexts[\"Wegmans\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        XCTAssertTrue(app.navigationBars["Wegmans"].staticTexts["Wegmans"].exists)
     }
 }
