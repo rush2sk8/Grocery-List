d88a29b06d6628541eb78b9c74945cee8c5979b4
Rushad Antia <rushadantia@gmail.com>
2020-07-21 17:43:48 -0400

    Update CollapsibleTableViewHeader.swift

--- Grocery List/Table/CollapsibleTableViewHeader.swift
+++ Grocery List/Table/CollapsibleTableViewHeader.swift
@@ -70,14 +70,3 @@ class CollapsibleTableViewHeader: UITableViewHeaderFooterView {
 protocol CollapsibleTableViewHeaderDelegate {
     func toggleSection(_ header: CollapsibleTableViewHeader, section: Int)
 }
-
-extension UIColor {
-    convenience init(hex: Int, alpha:CGFloat = 1.0){
-        self.init(
-            red: CGFloat((hex & 0xFF0000) >> 16) / 255 ,
-            green: CGFloat((hex & 0x00FF00) >> 8) / 255  ,
-            blue: CGFloat((hex & 0x0000FF) >> 0) / 255 ,
-            alpha: alpha
-        )
-    }
-}
