8eb609222d44520b61025345ce7cd3be1489afc1
rushadantia <rushadantia@gmail.com>
2020-09-01 20:40:24 -0400

    add removed and formatting

--- Grocery List/Data Models/Item.swift
+++ Grocery List/Data Models/Item.swift
@@ -9,55 +9,55 @@ import UIKit
 
 class Item: Codable {
     var imageString: String?
-    
+
     var hasImage: Bool {
         imageString != nil
     }
-    
+
     var name: String
     var isDone: Bool = false
     var isFavorite: Bool
-    
-    init(name: String, isFave: Bool, imageString: String, isDone: Bool){
+
+    init(name: String, isFave: Bool, imageString: String, isDone: Bool) {
         self.name = name.lowercased()
         self.imageString = imageString
         self.isDone = isDone
-        self.isFavorite = isFave
+        isFavorite = isFave
     }
-    
-    init(name: String, isFave: Bool, isDone: Bool){
+
+    init(name: String, isFave: Bool, isDone: Bool) {
         self.name = name.lowercased()
         self.isDone = isDone
-        self.isFavorite = isFave
+        isFavorite = isFave
     }
-    
+
     init(name: String) {
         self.name = name.lowercased()
-        self.isDone = false
-        self.isFavorite = false
+        isDone = false
+        isFavorite = false
     }
-    
+
     init(name: String, isFav: Bool) {
         self.name = name
-        self.isDone = false
-        self.isFavorite = isFav
+        isDone = false
+        isFavorite = isFav
     }
-    
-    init(){
-        self.name = ""
-        self.isDone = false
-        self.isFavorite = false
+
+    init() {
+        name = ""
+        isDone = false
+        isFavorite = false
     }
-    
+
     init(name: String, imageString: String) {
         self.name = name.lowercased()
         self.imageString = imageString
-        self.isDone = false
-        self.isFavorite = false
+        isDone = false
+        isFavorite = false
     }
-    
+
     func getImage() -> UIImage? {
-        if self.hasImage{
+        if hasImage {
             let newImageData = Data(base64Encoded: imageString!)
             if let img = newImageData {
                 return UIImage(data: img)
@@ -65,9 +65,9 @@ class Item: Codable {
         }
         return nil
     }
-    
+
     func getGreyImage() -> UIImage? {
-        if hasImage{
+        if hasImage {
             let ciImage = CIImage(image: getImage()!)
             let bw = ciImage!.applyingFilter("CIColorControls", parameters: ["inputSaturation": 0, "inputContrast": 1])
             return UIImage(ciImage: bw)
--- Grocery List/Data Models/Store.swift
+++ Grocery List/Data Models/Store.swift
@@ -10,176 +10,172 @@ import UIKit
 class Store: Codable {
     var name: String
     var imgName: String
-    
+
     var categories: [Category]
-    
-    init(name: String, img: String , _ categories: [Category] = [Category]()) {
+
+    init(name: String, img: String, _ categories: [Category] = [Category]()) {
         self.name = name
         self.categories = categories
-        self.imgName = img
+        imgName = img
     }
-    
+
     init() {
-        self.name = ""
-        self.categories = [Category]()
-        self.imgName = ""
+        name = ""
+        categories = [Category]()
+        imgName = ""
     }
-    
+
     // initializes with default categories
-    init(name: String){
+    init(name: String) {
         self.name = name
-        self.categories = [Category]()
-        self.imgName = ""
+        categories = [Category]()
+        imgName = ""
     }
-    
-    public func addItem(category: Category, item: Item){
+
+    public func addItem(category: Category, item: Item) {
         category.items.append(item)
         save()
     }
-    
-    public func addItem(category: String, item: Item){
+
+    public func addItem(category: String, item: Item) {
         getCategory(name: category)?.items.append(item)
         save()
     }
-    
-    public func editItem(category: Category, itemIndex: Int, newItemName: String){
+
+    public func editItem(category: Category, itemIndex: Int, newItemName: String) {
         category.items[itemIndex].name = newItemName
         save()
     }
-    
-    public func deleteItem(category: String, item: Item){
+
+    public func deleteItem(category: String, item: Item) {
         let items = getCategory(name: category)!.items
-        
+
         var index = -1
-        
-        for i in 0..<items.count {
+
+        for i in 0 ..< items.count {
             if items[i].name == item.name {
                 index = i
                 break
             }
         }
-        
+
         if index != -1 {
             getCategory(name: category)?.items.remove(at: index)
         }
-        
+
         save()
     }
-    
-    func addItemFromVoiceString(_ text: String){
+
+    func addItemFromVoiceString(_ text: String) {
         var strings = text.components(separatedBy: " ").map { x in x.lowercased() }
-        
-        if strings.contains("add"){
+
+        if strings.contains("add") {
             strings.remove(at: strings.firstIndex(of: "add")!)
         }
-        
+
         var index = -1
-        
-        if strings.contains("category")   {
+
+        if strings.contains("category") {
             index = strings.lastIndex(of: "category")!
-        }else if strings.contains("kategory") {
+        } else if strings.contains("kategory") {
             index = strings.lastIndex(of: "kategory")!
-        }else if strings.contains("under"){
+        } else if strings.contains("under") {
             index = strings.lastIndex(of: "under")!
-        }else {
-            self.addItem(category: "Other", item: Item(name: strings[0..<strings.count].joined(separator: " ").capitalized))
+        } else {
+            addItem(category: "Other", item: Item(name: strings[0 ..< strings.count].joined(separator: " ").capitalized))
             return
         }
-        
-        if index + 1  < strings.count {
-            var categoryString = strings[(index + 1)..<strings.count].joined(separator: " ").capitalized
+
+        if index + 1 < strings.count {
+            var categoryString = strings[(index + 1) ..< strings.count].joined(separator: " ").capitalized
             categoryString = categoryReplacement(categoryString)
-            
+
             if index - 1 >= 0 {
-                let itemName: String = strings[0..<index].joined(separator: " ").capitalized
-                
-                if self.getCategories().contains(categoryString.lowercased()){
-                    self.addItem(category: categoryString, item: Item(name: itemName))
+                let itemName: String = strings[0 ..< index].joined(separator: " ").capitalized
+
+                if getCategories().contains(categoryString.lowercased()) {
+                    addItem(category: categoryString, item: Item(name: itemName))
                 } else {
-                    self.addItem(category: "Other", item: Item(name: itemName))
+                    addItem(category: "Other", item: Item(name: itemName))
                 }
             }
         }
     }
-    
-    //TODO: move this to an alias field in the category
-    //this is horrible
+
+    // TODO: move this to an alias field in the category
+    // this is horrible
     private func categoryReplacement(_ categoryString: String) -> String {
-        if categoryString == "veggies"{
+        if categoryString == "veggies" {
             return "vegetables"
-        }
-        else if categoryString == "fruit" {
+        } else if categoryString == "fruit" {
             return "fruits"
-        }
-        else if categoryString == "meat"{
+        } else if categoryString == "meat" {
             return "meats"
-        }
-        else if categoryString == "frozen"{
+        } else if categoryString == "frozen" {
             return "frozen foods"
-        }
-        else {
+        } else {
             return categoryString
         }
     }
-    
+
     public func finishShopping() {
         for category in categories {
             category.pruneNonFavorites()
             category.resetFavorites()
         }
-        
+
         save()
     }
-    
+
     public func getCategories() -> [String] {
         var categories = [String]()
-        
+
         for c in self.categories {
             categories.append(c.name.lowercased())
         }
         return categories
     }
-    
+
     public func getNumNonDoneItems() -> Int {
         var total = 0
-        
-        for x in self.categories {
+
+        for x in categories {
             total += x.getNonDoneItems()
         }
         return total
     }
-    
+
     public func getNumItems() -> Int {
         var total = 0
-        
-        for c in self.categories {
+
+        for c in categories {
             total += c.items.count
         }
         return total
     }
-    
-    func addCategory(category: Category){
-        self.categories.append(category)
+
+    func addCategory(category: Category) {
+        categories.append(category)
     }
-    
+
     // gets a category from name
     func getCategory(name: String) -> Category? {
-        for c in self.categories {
+        for c in categories {
             if c.name == name {
                 return c
             }
         }
         return nil
     }
-    
-    //function that will export the current list to url to share
+
+    // function that will export the current list to url to share
     func exportToURL() -> URL? {
         guard let encoded = try? JSONEncoder().encode(self) else { return nil }
-        
-        let documents = FileManager.default.urls (for: .documentDirectory, in: .userDomainMask).first
-        
-        guard let path = documents?.appendingPathComponent("/\(self.name.lowercased()).groclst") else { return nil }
-        
+
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+
+        guard let path = documents?.appendingPathComponent("/\(name.lowercased()).groclst") else { return nil }
+
         do {
             try encoded.write(to: path, options: .atomicWrite)
             return path
@@ -188,11 +184,11 @@ class Store: Codable {
             return nil
         }
     }
-    
+
     public func getStoreImage() -> UIImage? {
-        return StoreIconManager.getImageFromString(imgString: self.imgName)
+        return StoreIconManager.getImageFromString(imgString: imgName)
     }
-    
+
     func save() {
         DataStore.saveStoreData(store: self)
     }
